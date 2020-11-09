f8a72abccba03ecdb3d4fe52660555e5520f1e1f
psh <qkrtjdgus268@naver.com>
2020-11-09 15:30:20 +0900

    bank selection (오른쪽 하단)

--- app/src/main/java/com/example/kbkbkb/MainActivity.java
+++ app/src/main/java/com/example/kbkbkb/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.kbkbkb;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,6 +10,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +24,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,14 +36,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+
+        final AlertDialog.Builder oDialog = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_Alert);
+        FloatingActionButton fab = findViewById(R.id.entry);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final CharSequence[] oItems = {"국민", "우리", "신한", "카카오뱅크", "농협", "농협", "농협"};
+                oDialog.setTitle("은행 선택")
+                        .setItems(oItems, new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                if ("국민".equals(oItems[which])) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com")));
+                                } else if ("우리".equals(oItems[which])) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com")));
+                                } else if ("신한".equals(oItems[which])) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com")));
+                                } else if ("카카오뱅크".equals(oItems[which])) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com")));
+                                } else if ("농협".equals(oItems[which])) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com")));
+                                }
+
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
