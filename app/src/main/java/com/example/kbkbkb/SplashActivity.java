725946f64a6f23a1edd9da66509023104de275be
psh <qkrtjdgus268@naver.com>
2020-11-10 15:33:49 +0900

    ...

---
+++ app/src/main/java/com/example/kbkbkb/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.kbkbkb;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+        finish();
+    }
+}
