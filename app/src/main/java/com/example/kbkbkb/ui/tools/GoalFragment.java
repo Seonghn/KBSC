612d98b89b737b5566ff741cbc9b1e72579fa014
psh <qkrtjdgus268@naver.com>
2020-11-25 15:56:51 +0900

    goal_popup

--- app/src/main/java/com/example/kbkbkb/ui/tools/GoalFragment.java
+++ app/src/main/java/com/example/kbkbkb/ui/tools/GoalFragment.java
@@ -1,10 +1,16 @@
 package com.example.kbkbkb.ui.tools;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -32,10 +38,68 @@ public class GoalFragment extends Fragment {
         //CircleIndicator 설정
 //        CircleIndicator indicator = (CircleIndicator) root.findViewById(R.id.consume_indicator);
 //        indicator.setViewPager(vpPager);
+        ImageButton add = root.findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OptionCodeTypeDialog octDialog  = new OptionCodeTypeDialog(getContext(), new CustomDialogClickListener() {
+                    @Override
+                    public void onPositiveClick() {
+                        Log.d("TEST", "OK click");
+                    }
 
+                    @Override
+                    public void onNegativeClick() {
+                        Log.d("TEST", "no click");
+                    }
+                });
+                octDialog.setCanceledOnTouchOutside(true);
+                octDialog.setCancelable(true);
+                octDialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+                octDialog.show();
+            }
+        });
         return root;
     }
+    public interface CustomDialogClickListener {
+        void onPositiveClick();
+        void onNegativeClick();
+    }
 
+    public class OptionCodeTypeDialog extends Dialog {
+
+        private Context context;
+        private CustomDialogClickListener customDialogClickListener;
+
+        public OptionCodeTypeDialog(@NonNull Context context, CustomDialogClickListener customDialogClickListener) {
+            super(context);
+            this.context = context;
+            this.customDialogClickListener = customDialogClickListener;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.goal_pop1);
+
+            Button yes = findViewById(R.id.yes);
+            yes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    customDialogClickListener.onPositiveClick();
+                    dismiss();
+                }
+            });
+            Button no = findViewById(R.id.no);
+            no.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    customDialogClickListener.onNegativeClick();
+                    dismiss();
+                }
+            });
+        }
+    }
 
     public static class MyPagerAdapter extends FragmentPagerAdapter {
         //fragment 개수 설정 변수
--- app/src/main/java/com/example/kbkbkb/ui/tools/GoalFragment1.java
+++ app/src/main/java/com/example/kbkbkb/ui/tools/GoalFragment1.java
@@ -1,16 +1,18 @@
 package com.example.kbkbkb.ui.tools;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.example.kbkbkb.R;
@@ -29,34 +31,8 @@ public class GoalFragment1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_goal1, container, false);//fragment layout 연결
 
-
-        ImageButton select = view.findViewById(R.id.select1);
-        select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GoalFragment.vpPager.setCurrentItem(2);
-            }
-        });
-
-
-        editText = view.findViewById(R.id.tg_mon);
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                GoalFragment.vpPager.setCurrentItem(2);
-            }
-        });
-
-        return view;
+            return view;
     }
+
+
 }
