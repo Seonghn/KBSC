7191c1f137087c4a1b6c12991d4f31cc6abffd8e
hanseul-Choi <58256559+hanseul-Choi@users.noreply.github.com>
2020-11-25 13:37:06 +0900

    오류 제거

--- app/src/main/java/com/example/kbkbkb/ui/tools/GoalFragment1.java
+++ app/src/main/java/com/example/kbkbkb/ui/tools/GoalFragment1.java
@@ -1,9 +1,13 @@
 package com.example.kbkbkb.ui.tools;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +16,10 @@ import androidx.fragment.app.Fragment;
 import com.example.kbkbkb.R;
 
 public class GoalFragment1 extends Fragment {
+
+    private Context mContext;
+
+    private ImageButton btn1;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,8 +31,21 @@ public class GoalFragment1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_goal1, container, false);//fragment layout 연결
 
+        btn1 = (ImageButton) view.findViewById(R.id.select1);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,"박성현 바보",Toast.LENGTH_LONG).show();
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 }
